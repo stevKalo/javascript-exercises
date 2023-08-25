0c82c70fa320c48d261c74ad1174872e4cde0b72
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-08-25 19:22:32 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let result = 0;
+  let x;
+  let y;
+  if (num1 < 0 || num2 < 0) {
+    result = "ERROR";
+  } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    result = "ERROR";
+  } else {
+    if (num1 < num2) {
+      x = num1;
+      y = num2;
+    } else if (num1 > num2) {
+      x = num2;
+      y = num1;
+    }
+    for (let i = x; i <= y; i++) {
+      result += i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
