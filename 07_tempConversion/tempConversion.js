478a341f5f74b5ccf1732c9efe93617d0dc2829b
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-08-25 22:09:53 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  newTemp = (temp - 32) * (5 / 9);
+  return Number(newTemp % 1 === 0 ? newTemp : newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+// (32°F − 32) × 5/9 = 0°C
+
+const convertToFahrenheit = function (temp) {
+  newTemp = temp * (9 / 5) + 32;
+  return Number(newTemp % 1 == 0 ? newTemp : newTemp.toFixed(1));
 };
+
+// (0°C × 9/5) + 32 = 32°F
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
