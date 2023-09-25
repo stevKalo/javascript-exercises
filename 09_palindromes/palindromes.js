10ab2c040de1a69713530368f692bf83f57d7839
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-09-25 11:26:58 -0400

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let str = string
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .replace(/\s/g, "")
+    .toLowerCase()
+    .split("");
+  if (str.join("") === str.reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
