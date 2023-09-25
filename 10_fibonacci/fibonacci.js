926c8e1f03fc8c94f5cce9520333f85ec141dbb8
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-09-25 17:09:01 -0400

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  let array = [1, 1];
+  let fib;
+  for (let i = 2; i < num; i++) {
+    fib = array.at(-1) + array.at(-2);
+    array.push(fib);
+  }
+  return array.at(-1);
 };
 
 // Do not edit below this line
