90f527792147d4034b4718c804cc254bc6f9be56
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-09-27 14:10:02 -0400

    Complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  return arr.map((book) => book.title);
 };
+// make names array
+// for each obj in array
+// get obj.title (array[0].title?)
+// push to names array
 
 // Do not edit below this line
 module.exports = getTheTitles;
