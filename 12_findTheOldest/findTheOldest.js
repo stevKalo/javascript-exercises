792d635d753b4d445a796eba311d9f42cf34a26e
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-09-27 14:46:26 -0400

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
+// make get age function
+// function compares object birth and death years
+// uses current year if still alive new Date().getFullYear()
+
+// reduce arr
+// oldest vs current person
+// compare ages with getAge
+// assign oldest to obj with greater age
+// return obj.name of oldest person
 // Do not edit below this line
 module.exports = findTheOldest;
